4af9d621b21c1b764b48824fce1e4e43d16381a4
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2021-10-24 19:31:33 -0700

    enabled rcon to permemently store variabels (UNSTABLE VERSION)

--- rcon.js
+++ rcon.js
@@ -3,32 +3,82 @@
 // You need to run `npm install keypress` for this example to work.
 
 const commander = require('commander');
-var Rcon = require('rcon');
-//var rcon = new Rcon('localhost', 1234, 'password');
-var authenticated = false;
-var queuedCommands = [];
-const program = new commander.Command();
+const Rcon = require('rcon');
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
-})
-console.log('Authenticating...')
+});
+const fs = require('fs');
+//var credentials = require('./credentials.json');
+var rcon = new Rcon(null, null, null);
+var authenticated = false;
+var queuedCommands = [];
+const program = new commander.Command();
+immediateTermination = false;
+
+
 program
-.showHelpAfterError()
-.showSuggestionAfterError()
-  .option('-P, --port <port>', 'choose the port', '25575')
-  .option('-H, --host <host>', 'choose the host ip', 'localhost')
-  .option('-p, --password <password>', 'specify the rcon password', 'password')
+  //.arguments("[connect|config|run <command>]")
+  .showHelpAfterError()
+  .showSuggestionAfterError()
+  .option('-p, --port <port>', 'This is the port that the Rcon connection will be established on', (p) => storeValue("port", p))
+  .option('-h, --host <host>', 'This is the host that the Rcon will connect to', (p) => storeValue("host", p))
+  .option('-P, --password <password>', 'specify the rcon password', (p) => storeValue("password", p))
+  //.argument('config',  "This will open the configuration menu",() => openConfig())
+  .argument('run <command>', "this will run your specified command then close the connection", (i) => sendCommand(i))
   .parse();
-var rcon = new Rcon(program.opts().host, program.opts().port, program.opts().password);
-rcon.connect();
-//rcon.send(process.stdin);
-rcon.on('auth', function() {
-  console.log("Authenticated");
+
+function sendCommand(cmd) {
+  immediateTermination = true;
+  rcon = new Rcon(getValue("host"), getValue("port"), getValue("password"));
+  queuedCommands.push(cmd);
+  rcon.connect();
+  //rcon.send(program.opts().run);
+}
+function nothing() {
+
+}
+function openConfig() {
+  readline.question('What port would you like to use? \n', port => {
+    cache.set("port", port);
+  });
+  readline.question('\nWhat host would you like to connect to? \n', host => {
+    cache.set("host", host);
+  });
+  readline.question('\nWhat is the rcon password? \n', password => {
+    cache.set("password", password)
+  });
+  console.log("Successfully configured!");
+  process.exit();
+}
+function storeValue(id, value) {
+  console.log("Updating " + id + " to " + value)
+  switch (id) {
+    case 'port': {
+      item = { port: value, host: getValue("host"), password: getValue("password")};
+    }
+    case 'host': {
+      item = { host: value, password: getValue("password") , port: getValue("port") };
+    }
+    case 'password': {
+      item = { password: value , port: getValue("port"), host: getValue("host")};
+    }
+  }
+  fs.writeFileSync('./credentials.json', JSON.stringify(item), err => { if (err) console.error(err) });
+}
+function getValue(id) {
+  results = JSON.parse(fs.readFileSync('./credentials.json', (err, data) => results = JSON.parse));
+  results = require('./credentials.json');
+  //console.log(results);
+  return results[id];
+}
+
+rcon.on('auth', function () {
+  console.log("Connected");
   readline.question(`> `, command => {
-    if(command == 'close' || command == 'exit') process.exit();
+    if (command == 'close' || command == 'exit') process.exit();
     rcon.send(command)
-//    readline.close()
+    //    readline.close()
   })
   authenticated = true;
 
@@ -42,19 +92,19 @@ rcon.on('auth', function() {
     rcon.send(queuedCommands[i]);
   }
   queuedCommands = [];
-
-}).on('response', function(str) {
+  if(immediateTermination) process.exit();
+}).on('response', function (str) {
   console.log(str);
   readline.question(`> `, command => {
-    if(command == 'close' || command == 'exit') process.exit();
+    if (command == 'close' || command == 'exit') process.exit();
     rcon.send(command)
-//    readline.close()
+    //    readline.close()
   })
-}).on('error', function(err) {
-  console.log("Error: " + err);
-}).on('end', function() {
+}).on('error', function (err) {
+  console.error(err);
+  console.error("host: " + getValue("host") + ", port: " + getValue("port") + ", password: " + getValue("password"));
+}).on('end', function () {
   readline.close();
-  console.log("Connection closed");
+  console.info("Connection closed");
   process.exit();
 });
-
