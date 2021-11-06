97d54215fc0d406318e6a170c27fafc2925d43f1
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2021-11-06 08:01:53 -0700

    Semi-Stable release, use with caution - added config.json file - added --save argument - added loading icons for authentication

--- rcon.js
+++ rcon.js
@@ -1,85 +1,109 @@
 #!/usr/bin/env node
-// This example reads commands from stdin and sends them on enter key press.
-// You need to run `npm install keypress` for this example to work.
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.rconConsole = void 0;
+process.title = 'rcon';
 const commander = require('commander');
 const Rcon = require('rcon');
+const child_process = require("child_process");
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
 const fs = require('fs');
+const version = require('./package.json').version;
+const ora = require("ora");
+var authenticate = ora({text: 'Authenticating...', discardStdin: false, color: "yellow"});
+var command = ora({text:"Something went wrong, please try again", discardStdin: false, color: "cyan"});
+
 //var credentials = require('./credentials.json');
-var rcon = new Rcon(null, null, null);
+var config = require('./config.json');
 var authenticated = false;
 var queuedCommands = [];
 const program = new commander.Command();
-immediateTermination = false;
+const rcon = new Rcon(config.host, config.port, config.password);
+class rconConsole {
+  constructor() {
+    commander
+      .showHelpAfterError()
+      .showSuggestionAfterError()
+      .version(version)
+      .arguments('[reset|uninstall|rebuild|help]')
+      .option('-H, --host <host>', 'This is the host that the Rcon will connect to (default: ' + config.host + ")", (p) => this.cache("host", p))
+      .option('-P, --port <port>', 'This is the port that the Rcon connection will be established on (default: ' + config.port + ")", (p) => config.port = p)
+      .option('-p, --password <password>', 'specify the rcon password (default: ' + config.password + ")", (p) => config.password = p)
+      .option('--tcp <true/false>', 'choose which rcon protocol to use, true for TCP and False for UDP (default: ' + config.options.tcp + ")", (p) => config.options.tcp = p)
+      .option('-s, --save', 'save your port, host, and password to the config.json', () => this.updateConfig())
+      .action((cmd) => {
+        this.action = cmd;
+      })
+      .parse();
+    switch (this.action) {
+      case 'reset': {
+        console.log('Reseting...');
+        fs.writeFileSync('./config.json', JSON.stringify(require('./default_config.json'), null, 2));
+        process.exit(1);
+      }
+      case 'uninstall': {
+        console.log('Uninstalling...');
+        child_process.exec('npm uninstall rcon-console', (error) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+          }
+          console.log(`successfully uninstalled rcon-console`);
+          process.exit(1);
+        });
+        process.exit(1);
+      }
+      case 'rebuild': {
+        console.log('Rebuilding...');
+        child_process.exec('npm install rcon-console', (error) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+          }
+          console.log(`successfully rebuilt rcon-console`);
+          process.exit(1);
+        });
+        break;
+      }
+      case 'help': {
+        commander.outputHelp();
+        console.log('Please provide a command:');
+        console.log('    reset                            resets the configuration');
+        console.log('    uninstall                        uninstall the program from your device');
+        console.log('    rebuild                          rebuild all your files (use after updating)');
+        console.log('\nSee the wiki for help with hb-service: https://git.io/JTtHK \n');
+        process.exit(1);
+      }
+      default: {
+        authenticate.start("Authenticating...");
+        rcon.connect();
 
-
-program
-  //.arguments("[connect|config|run <command>]")
-  .showHelpAfterError()
-  .showSuggestionAfterError()
-  .option('-p, --port <port>', 'This is the port that the Rcon connection will be established on', (p) => storeValue("port", p))
-  .option('-h, --host <host>', 'This is the host that the Rcon will connect to', (p) => storeValue("host", p))
-  .option('-P, --password <password>', 'specify the rcon password', (p) => storeValue("password", p))
-  //.argument('config',  "This will open the configuration menu",() => openConfig())
-  .argument('run <command>', "this will run your specified command then close the connection", (i) => sendCommand(i))
-  .parse();
-
-function sendCommand(cmd) {
-  immediateTermination = true;
-  rcon = new Rcon(getValue("host"), getValue("port"), getValue("password"));
-  queuedCommands.push(cmd);
-  rcon.connect();
-  //rcon.send(program.opts().run);
-}
-function nothing() {
-
-}
-function openConfig() {
-  readline.question('What port would you like to use? \n', port => {
-    cache.set("port", port);
-  });
-  readline.question('\nWhat host would you like to connect to? \n', host => {
-    cache.set("host", host);
-  });
-  readline.question('\nWhat is the rcon password? \n', password => {
-    cache.set("password", password)
-  });
-  console.log("Successfully configured!");
-  process.exit();
-}
-function storeValue(id, value) {
-  console.log("Updating " + id + " to " + value)
-  switch (id) {
-    case 'port': {
-      item = { port: value, host: getValue("host"), password: getValue("password")};
+      }
     }
-    case 'host': {
-      item = { host: value, password: getValue("password") , port: getValue("port") };
-    }
-    case 'password': {
-      item = { password: value , port: getValue("port"), host: getValue("host")};
-    }
+
   }
-  fs.writeFileSync('./credentials.json', JSON.stringify(item), err => { if (err) console.error(err) });
+  cache(key, value) {
+    console.log("Setting " + key + " to " + value);
+    config[key] = value;
+    rcon[key] = value;
+  }
+  updateConfig() {
+    //config.host = "10.0.0.5";
+    console.log("Updating Configuration...")
+    fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
+    config = require('./config.json');
+  }
 }
-function getValue(id) {
-  results = JSON.parse(fs.readFileSync('./credentials.json', (err, data) => results = JSON.parse));
-  results = require('./credentials.json');
-  //console.log(results);
-  return results[id];
-}
+readline.on('line', (content) => {
+  if (authenticated) rcon.send(content); else queuedCommands.push(content);
+  command.start(content);
+});
 
 rcon.on('auth', function () {
-  console.log("Connected");
-  readline.question(`> `, command => {
-    if (command == 'close' || command == 'exit') process.exit();
-    rcon.send(command)
-    //    readline.close()
-  })
+  authenticate.succeed('Authenticated');
   authenticated = true;
 
   // You must wait until this event is fired before sending any commands,
@@ -89,22 +113,24 @@ rcon.on('auth', function () {
   // them all once the connection is available.
 
   for (var i = 0; i < queuedCommands.length; i++) {
+    console.log("Queued Command Sent: " + queuedCommands[i]);
     rcon.send(queuedCommands[i]);
   }
+
   queuedCommands = [];
-  if(immediateTermination) process.exit();
 }).on('response', function (str) {
-  console.log(str);
-  readline.question(`> `, command => {
-    if (command == 'close' || command == 'exit') process.exit();
-    rcon.send(command)
-    //    readline.close()
-  })
+  if(str.includes("Unknown or incomplete command, see below for error")) command.fail("Unknown or incomplete command, see below for error\n"+str.slice(50)); else command.succeed(str);
 }).on('error', function (err) {
+  if (!authenticated) authenticate.fail("Error: Authentication failed"); else command.fail("Error encountered");
   console.error(err);
-  console.error("host: " + getValue("host") + ", port: " + getValue("port") + ", password: " + getValue("password"));
+  console.error("host: " + config.host + ", port: " + config.port + ", password: " + config.password);
 }).on('end', function () {
-  readline.close();
-  console.info("Connection closed");
-  process.exit();
+  console.log("Connection closed");
+  authenticated = false;
+  //process.exit();
 });
+exports.rconConsole = rconConsole;
+function bootstrap() {
+  return new rconConsole();
+}
+bootstrap();
