dd7f5012bd71e513db692ce2d73dd83720c1c0a7
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2021-11-07 15:39:45 -0800

    Stable release, dont use the config commnd (WIP)

--- rcon.js
+++ rcon.js
@@ -11,95 +11,155 @@ const readline = require('readline').createInterface({
   output: process.stdout
 });
 const fs = require('fs');
+const semver = require('semver');
+var outOfDate;
+var latestVersion;
 const version = require('./package.json').version;
-const ora = require("ora");
-var authenticate = ora({text: 'Authenticating...', discardStdin: false, color: "yellow"});
-var command = ora({text:"Something went wrong, please try again", discardStdin: false, color: "cyan"});
+const checkForUpdates = child_process.execSync('npm view rcon-console version', (error, stdout) => {
+  if (error) {
+    return error;
+  } else if (semver.gte(semver.clean(version), semver.clean(stdout))) {
+    outOfDate = false;
+  } else {
+    outOfDate = true;
+    console.log('you are a %s version behind, run `rcon update` to update', semver.diff(semver.clean(version), semver.clean(stdout)));
+  }
+  latestVersion = semver.clean(stdout);
+});
 
+const ora = require("ora");
+var authenticate = ora({ text: 'Authenticating...', discardStdin: false, color: "yellow" });
+var command = ora({ text: "Something went wrong, please try again", discardStdin: false, color: "cyan" });
+var action = ora({ discardStdin: false, color: "green" });
 //var credentials = require('./credentials.json');
 var config = require('./config.json');
+const questions = require('./configuration_questions.json');
+var question = 0;
+const { emit, stdin } = require('process');
 var authenticated = false;
 var queuedCommands = [];
 const program = new commander.Command();
-const rcon = new Rcon(config.host, config.port, config.password);
+const rcon = new Rcon(config.host, config.port, config.password, config.options);
+var configuring = false;
 class rconConsole {
   constructor() {
     commander
       .showHelpAfterError()
       .showSuggestionAfterError()
       .version(version)
-      .arguments('[reset|uninstall|rebuild|help]')
-      .option('-H, --host <host>', 'This is the host that the Rcon will connect to (default: ' + config.host + ")", (p) => this.cache("host", p))
-      .option('-P, --port <port>', 'This is the port that the Rcon connection will be established on (default: ' + config.port + ")", (p) => config.port = p)
-      .option('-p, --password <password>', 'specify the rcon password (default: ' + config.password + ")", (p) => config.password = p)
-      .option('--tcp <true/false>', 'choose which rcon protocol to use, true for TCP and False for UDP (default: ' + config.options.tcp + ")", (p) => config.options.tcp = p)
-      .option('-s, --save', 'save your port, host, and password to the config.json', () => this.updateConfig())
+      .arguments('[reset|uninstall|rebuild|help|config]')
+      .addHelpText('after', `
+      Please provide a command:
+
+          reset                            resets the configuration
+          uninstall                        uninstall the program from your device
+          rebuild                          rebuild rcon-console (use if crashing)
+          update                           updates rcon-console
+          config                           update your config.json values
+
+          See the wiki for help: https://github.com/bloomkd46/rcon-console/wiki
+      `)
+      .option('-H, --host <host>', 'This is the host that the Rcon will connect to (default: ' + config.host + ")", (p) => cache("host", p))
+      .option('-P, --port <port>', 'This is the port that the Rcon connection will be established on (default: ' + config.port + ")", (p) => cache("port", p))
+      .option('-p, --password <password>', 'specify the rcon password (default: ' + config.password + ")", (p) => cache("password", p))
+      .option('--tcp <true/false>', 'choose which rcon protocol to use, true for TCP and False for UDP (default: ' + config.options.tcp + ")", (p) => cache("options", p, "tcp"))
+      .option('-C, --challenge <true/false>', 'choose wether to use the rcon challenge protocal (default: ' + config.options.challenge + ")", (p) => cache("options", p, "challenge"))
+      .option('-S, --save', 'save your port, host, and password to the config.json', () => updateConfig())
       .action((cmd) => {
         this.action = cmd;
       })
       .parse();
     switch (this.action) {
       case 'reset': {
-        console.log('Reseting...');
-        fs.writeFileSync('./config.json', JSON.stringify(require('./default_config.json'), null, 2));
+        action.start("Reseting configuration...");
+        try {
+          fs.writeFileSync('./config.json', JSON.stringify(require('./default_config.json'), null, 2));
+          action.succeed("configuration successfully reset");
+        } catch (error) {
+          action.fail("Error: Unable to reset configuration\n" + error);
+        }
         process.exit(1);
       }
       case 'uninstall': {
-        console.log('Uninstalling...');
+        action.start("Uninstalling...");
         child_process.exec('npm uninstall rcon-console', (error) => {
           if (error) {
-            console.error(`exec error: ${error}`);
+            action.fail("Error: Unable to uninstall rcon-console\n" + error);
             return;
+          } else {
+            action.succeed("Successfully uninstalled rcon-console");
           }
-          console.log(`successfully uninstalled rcon-console`);
-          process.exit(1);
         });
         process.exit(1);
       }
       case 'rebuild': {
-        console.log('Rebuilding...');
-        child_process.exec('npm install rcon-console -g', (error) => {
+        action.start('Rebuilding...');
+        child_process.exec('npm install -g rcon-console@' + version, (error) => {
           if (error) {
-            console.error(`exec error: ${error}`);
+            action.fail(`exec error: ${error}`);
             return;
           }
-          console.log(`successfully rebuilt rcon-console`);
+          action.succeed(`successfully rebuilt rcon-console`);
           process.exit(1);
         });
         break;
       }
+      case 'update': {
+        action.start("Updating from v" + version + " to v" + latestVersion);
+        if (outOfDate) {
+          child_process.exec('npm install -g rcon-console@' + latestVersion, (error) => {
+            if (error) {
+              action.fail(`exec error: ${error}`);
+              return;
+            }
+            action.succeed(`successfully updated rcon-console to v` + latestVersion);
+            process.exit(1);
+          });
+        } else {
+          action.fail("Already Up-To-Date with v" + version);
+        }
+        process.exit(1);
+      }
       case 'help': {
         commander.outputHelp();
-        console.log('Please provide a command:');
-        console.log('    reset                            resets the configuration');
-        console.log('    uninstall                        uninstall the program from your device');
-        console.log('    rebuild                          rebuild all your files (use after updating)');
-        console.log('\nSee the wiki for help with hb-service: https://git.io/JTtHK \n');
-        process.exit(1);
+        break;
+      }
+      case 'config': {
+        configuring = true;
+        action.info("what would you like to set the "+ questions[question+1] + " to?");
+        break;
       }
       default: {
         authenticate.start("Authenticating...");
         rcon.connect();
-
+        if (this.action) {
+          command.start(this.action);
+          if (authenticated) rcon.send(this.action); else queuedCommands.push(this.action);
+        }
       }
     }
 
   }
-  cache(key, value) {
-    console.log("Setting " + key + " to " + value);
-    config[key] = value;
-    rcon[key] = value;
-  }
-  updateConfig() {
-    //config.host = "10.0.0.5";
-    console.log("Updating Configuration...")
-    fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
-    config = require('./config.json');
-  }
+
 }
 readline.on('line', (content) => {
-  if (authenticated) rcon.send(content); else queuedCommands.push(content);
-  command.start(content);
+  if (configuring) {
+    if (question > 5) { updateConfig(); process.exit(1); }
+    if (question >= 4) {
+      question++;
+      cache("options", content, questions[question+1]);
+      if (question == 5) action.info("would you like to use the challeng protocal? (true/false)");
+    } else {
+      question++;
+      cache(questions[question], content);
+      if (question == 4) action.info("would you like to use TCP or UDP? (true for TCP, false for UDP)");
+      action.info("what would you like to set the "+ questions[question+1] + " to?");
+    }
+    console.log(questions[question + 1]);
+  } else {
+    command.start(content);
+    if (authenticated) rcon.send(content); else queuedCommands.push(content);
+  }
 });
 
 rcon.on('auth', function () {
@@ -113,22 +173,54 @@ rcon.on('auth', function () {
   // them all once the connection is available.
 
   for (var i = 0; i < queuedCommands.length; i++) {
-    console.log("Queued Command Sent: " + queuedCommands[i]);
+    command.start(queuedCommands[i]);
     rcon.send(queuedCommands[i]);
   }
 
   queuedCommands = [];
 }).on('response', function (str) {
-  if(str.includes("Unknown or incomplete command, see below for error")) command.fail("Unknown or incomplete command, see below for error\n"+str.slice(50)); else command.succeed(str);
+  if (str.includes("Unknown or incomplete command, see below for error")) command.fail("Unknown or incomplete command, see below for error\n" + str.slice(50)); else command.succeed(str);
 }).on('error', function (err) {
-  if (!authenticated) authenticate.fail("Error: Authentication failed"); else command.fail("Error encountered");
-  console.error(err);
-  console.error("host: " + config.host + ", port: " + config.port + ", password: " + config.password);
+  if (!authenticated) authenticate.fail("Authentication failed\n" + JSON.stringify(err, null, 2)); else command.fail("Error encountered: " + err);
 }).on('end', function () {
-  console.log("Connection closed");
+  console.log("Connection closed, Reconnecting...");
   authenticated = false;
+  authenticate.start("Authenticating...");
+  rcon.connect();
   //process.exit();
 });
+function cache(key, value, _key2) {
+  action.start("Setting " + key + " to " + value);
+  try {
+    if (_key2) {
+      config[key][_key2] = value;
+      rcon[key + "." + _key2] = value;
+      action.succeed("Successfully set " + key + " to " + value);
+    } else {
+      config[key] = value;
+      rcon[key] = value;
+      action.succeed("Successfully set " + key + " to " + value);
+    }
+  } catch (error) {
+    if (_key2) {
+      action.fail("Failed to set " + key + "." + _key2 + " to " + value + "\n" + error);
+    } else {
+      action.fail("Failed to set " + key + " to " + value + "\n" + error);
+    }
+  }
+
+}
+function updateConfig() {
+  //config.host = "10.0.0.5";
+  action.start("Updating Configuration...");
+  try {
+    fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
+    config = require('./config.json');
+    action.succeed("Successfully updated configuration");
+  } catch (error) {
+    action.fail("Failed to update configuration, " + error);
+  }
+}
 exports.rconConsole = rconConsole;
 function bootstrap() {
   return new rconConsole();
